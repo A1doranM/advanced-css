374aac85a0e4c26b1eb5af15b5b8e11decd7f42c
A1doranM <vaselisk1997@gmail.com>
2021-01-29 17:48:03 +0200

    create footer, header, realtors, and sidebar

--- Nexter_m/react-ts-version/src/components/Footer.tsx
+++ Nexter_m/react-ts-version/src/components/Footer.tsx
@@ -3,7 +3,17 @@ import React from "react";
 export const Footer: React.FC = () => {
     return (
         <footer className="footer">
-            Footer
+            <ul className="nav">
+                <li className="nav__item"><a href="#" className="nav__link">Find your dream home</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Request proposal</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Download home planner</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Come work with us</a></li>
+            </ul>
+            <p className="copyright">&copy; Copyright test text Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Alias aut cum cumque iure modi molestias, quibusdam similique velit! Cum dicta dolore eaque facere illum
+                libero, mollitia quaerat repellendus similique veniam.</p>
         </footer>
     );
 };
--- Nexter_m/react-ts-version/src/components/Header.tsx
+++ Nexter_m/react-ts-version/src/components/Header.tsx
@@ -3,7 +3,17 @@ import React from "react";
 export const Header: React.FC = () => {
     return (
         <header className="header">
-            Header
+            <img src="logo.png" alt="Nexter" className="header__logo"/>
+            <h3 className="heading-3">Your own home:</h3>
+            <h1 className="heading-1">The ultimate personal freedom</h1>
+            <button className="btn header__btn">View our properties</button>
+            <div className="header__seenon-text">Seen on</div>
+            <div className="header__seenon-logos">
+                <img src="logo-bbc.png" alt=""/>
+                <img src="logo-forbes.png" alt=""/>
+                <img src="logo-techcrunch.png" alt=""/>
+                <img src="logo-bi.png" alt=""/>
+            </div>
         </header>
     );
 };
